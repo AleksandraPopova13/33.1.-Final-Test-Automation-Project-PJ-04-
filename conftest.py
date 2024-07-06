6caa9d080453b88a166989ed70cc58eb2d33eefd
Aleksandra <157876297+AleksandraPopova13@users.noreply.github.com>
2024-07-06 12:53:21 +0400

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,114 @@
+import pytest
+import allure
+import uuid
+
+@pytest.fixture(scope="function")
+def chrome_browser_instance(request):
+    options = webdriver.ChromeOptions()
+    browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+    yield browser
+    log_file = os.path.join(os.getcwd(), "cookie_log.txt")
+    with open(log_file, "w") as f:
+        before_cookies = browser.get_cookies()
+        f.write("Cookies before deletion:\n")
+        for cookie in before_cookies:
+            f.write(str(cookie) + "\n")
+        browser.delete_all_cookies()
+        after_cookies = browser.get_cookies()
+        f.write("Cookies after deletion:\n")
+        for cookie in after_cookies:
+            f.write(str(cookie) + "\n")
+    browser.quit()
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--log-level=DEBUG')
+
+    return chrome_options
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    # This function helps to detect that some test failed
+    # and pass this information to teardown:
+
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+
+@pytest.fixture
+def web_browser(request, selenium):
+
+    browser = selenium
+    browser.set_window_size(1400, 1000)
+
+    # Return browser instance to test case:
+    yield browser
+
+    # Do teardown (this code will be executed after each test):
+
+    if request.node.rep_call.failed:
+        # Make the screen-shot if test failed:
+        try:
+            browser.execute_script("document.body.bgColor = 'white';")
+
+            # Make screen-shot for local debug:
+            browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
+
+            # Attach screenshot to Allure report:
+            allure.attach(browser.get_screenshot_as_png(),
+                          name=request.function.__name__,
+                          attachment_type=allure.attachment_type.PNG)
+
+            # For happy debugging:
+            print('URL: ', browser.current_url)
+            print('Browser logs:')
+            for log in browser.get_log('browser'):
+                print(log)
+
+        except:
+            pass # just ignore any errors here
+
+
+def get_test_case_docstring(item):
+
+    full_name = ''
+
+    if item._obj.__doc__:
+        name = str(item._obj.__doc__.split('.')[0]).strip()
+        full_name = ' '.join(name.split())
+
+        if hasattr(item, 'callspec'):
+            params = item.callspec.params
+
+            res_keys = sorted([k for k in params])
+            # Create List based on Dict:
+            res = ['{0}_"{1}"'.format(k, params[k]) for k in res_keys]
+            # Add dict with all parameters to the name of test case:
+            full_name += ' Parameters ' + str(', '.join(res))
+            full_name = full_name.replace(':', '')
+
+    return full_name
+
+
+def pytest_itemcollected(item):
+
+
+    if item._obj.__doc__:
+        item._nodeid = get_test_case_docstring(item)
+
+
+def pytest_collection_finish(session):
+
+
+    if session.config.option.collectonly is True:
+        for item in session.items:
+            if item._obj.__doc__:
+                full_name = get_test_case_docstring(item)
+                print(full_name)
+
+        pytest.exit('Done!')
